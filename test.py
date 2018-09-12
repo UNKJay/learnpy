ee4b386e6b83c1a648a8f76e4c8457823ccb3073
pkj <1417752158@qq.com>
2018-09-12 12:19:14 +0800

    9/12 12:19

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
-import json
+import re
 
-obj = dict(name='pkj', age=20)
-s = json.dumps(obj, ensure_ascii=True)
-print(s)
+re_email=re.compile(r'^([<>\w\s]+)@([\w]+).org$')
+
+def is_valid_email(addr):
+    return re_email.match(addr)
+
+def name_of_email(addr):
+    result=re_email.match(addr).group(1)
+    result=re.split(r'[<>]',result)
+    if result[0]:
+        return result[0]
+    else:
+        return result[1]
+    
+
+
+# 测试:
+assert name_of_email('<Tom Paris> tom@voyager.org') == 'Tom Paris'
+assert name_of_email('tom@voyager.org') == 'tom'
+print('ok')
