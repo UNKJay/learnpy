dead72a0aee0c7afe22d1f239d0f2a7717ebc6a3
pkj <1417752158@qq.com>
2018-09-15 20:23:07 +0800

    9/15 20:23

--- test.py
+++ test.py
@@ -1,21 +1,34 @@
-import re
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
-re_email=re.compile(r'^([<>\w\s]+)@([\w]+).org$')
+import random
 
-def is_valid_email(addr):
-    return re_email.match(addr)
+# 随机字母:
+def rndChar():
+    return chr(random.randint(65, 90))
 
-def name_of_email(addr):
-    result=re_email.match(addr).group(1)
-    result=re.split(r'[<>]',result)
-    if result[0]:
-        return result[0]
-    else:
-        return result[1]
-    
+# 随机颜色1:
+def rndColor():
+    return (random.randint(64, 255), random.randint(64, 255), random.randint(64, 255))
 
+# 随机颜色2:
+def rndColor2():
+    return (random.randint(32, 127), random.randint(32, 127), random.randint(32, 127))
 
-# 测试:
-assert name_of_email('<Tom Paris> tom@voyager.org') == 'Tom Paris'
-assert name_of_email('tom@voyager.org') == 'tom'
-print('ok')
+# 240 x 60:
+width = 60 * 4
+height = 60
+image = Image.new('RGB', (width, height), (255, 255, 255))
+# 创建Font对象:
+font = ImageFont.truetype('/usr/share/wingding.ttf', 36)
+# 创建Draw对象:
+draw = ImageDraw.Draw(image)
+# 填充每个像素:
+for x in range(width):
+    for y in range(height):
+        draw.point((x, y), fill=rndColor())
+# 输出文字:
+for t in range(4):
+    draw.text((60 * t + 10, 10), rndChar(), font=font, fill=rndColor2())
+# 模糊:
+#image = image.filter(ImageFilter.BLUR)
+image.save('code.jpg', 'jpeg')
